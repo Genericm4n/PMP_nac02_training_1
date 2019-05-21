8fa2b9e0cd5561df1fe5015c34aaebef8cc99ce6
Júlio "genericman" César <47559029+Genericm4n@users.noreply.github.com>
2019-05-21 09:44:33 -0300

    Gerador de helicopteros

---
+++ Assets/Scripts/HelicopterGerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterGerator : MonoBehaviour {
+
+    // declarando variavel publica com o prefab do  helicoptero
+    public GameObject heliPrefab;
+    // variavel float com o delay para gerar um novo helicoptero
+    public float delay = 2.0f;
+
+	void Start ()
+    {
+        InvokeRepeating("GerateHelicopter", delay, delay);
+	}
+	
+	void GerateHelicopter()
+    {
+        GameObject helicopter = Instantiate(heliPrefab);
+
+        int randomNum = Random.Range(0, 4);
+
+        helicopter.transform.eulerAngles = Vector3.up * randomNum * 90;
+	}
+}
